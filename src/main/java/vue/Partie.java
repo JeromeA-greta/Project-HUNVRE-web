75d67b9c9a670dbbe5feb43cb7e1185ea64a8a62
AllanC229 <allan.chvt@gmail.com>
2026-05-11 10:17:49 +0200

    Ajout de la vue Partie et du ControleurPartie

---
+++ src/main/java/vue/Partie.java
@@ -0,0 +1,50 @@
+package vue;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import controleur.ControleurPartie;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Utilisateur;
+
+@WebServlet("/Partie")
+public class Partie extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+		
+		PrintWriter out=response.getWriter();
+		
+		HttpSession h = request.getSession(false);
+		Utilisateur joueur = (Utilisateur) h.getAttribute("joueur");
+		
+		if (h == null || h.getAttribute("joueur") == null) { //Si la session n'existe pas, renvoie vers la page de connexion
+		    response.sendRedirect("/Connexion");
+		    return;
+		}
+		
+	//	ControleurPartie controleurpartie = new ControleurPartie();
+		
+		out.print("<!Doctype html>"
+				+ "<html>"
+				+ "<head>"
+				+ "<meta charset=\"utf-8\"/> \r\n"
+				+ "<link rel='stylesheet' href='style.css'>"
+				+ "</head>"
+				+ "<body> <div id='partie'>"
+				+ "<table>"
+				+ "<tr><td id='00'></td><td id='01'></td><td id='02'></td></tr>"
+				+ "<tr><td id='10'></td><td id='11'></td><td id='12'></td></tr>"
+				+ "<tr><td id='20'></td><td id='21'></td><td id='22'></td></tr>"
+				+ "</table>"				
+				+ "</body>"
+				+ "</html>");
+
+	}
+}
